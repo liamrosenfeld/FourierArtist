ddd62e3fdc7e69af58bfa0ca373ea489a64e017f
Liam Rosenfeld <liamrosenfeld@users.noreply.github.com>
2019-03-17 23:14:10 -0400

    Inverse Checker
    Python to the Rescue

---
+++ InverseChecker/README.md
@@ -0,0 +1,3 @@
+# Fourier Artist: Inverse Checker
+
+This is a python project that checks that the inverse fourier transform is correct
---
+++ InverseChecker/from_waves.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from waves import *
+
+waves = getWaves()
+theta = 0
+x = []
+y = []
+while theta <= 2 * np.pi:
+    xValue = 0
+    yValue = 0
+    for w in waves:
+        phi = w.freq * theta + w.phase
+        xValue += w.amp * np.cos(phi)
+        yValue += w.amp * np.sin(phi)
+    x.append(xValue)
+    y.append(yValue)
+    theta += (2 * np.pi) / len(waves)
+
+plt.plot(x, y, marker='o')
+plt.xscale = np.pi
+plt.title("Swift Logo")
+plt.show()
